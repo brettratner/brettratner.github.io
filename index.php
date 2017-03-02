274bdf1e15b97cfe0294e3c995dde02fa3c9bba1
brettratner <brettratner@users.noreply.github.com>
2017-03-02 08:22:35 -0500

    Created Index file

---
+++ index.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Brett Ratner</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/creative.css">
+    <link rel="stylesheet" type="text/css" href="css/1-col-portfolio.css">
+    <link rel="stylesheet" type="text/css" href="css/lightbox.css">
+    <link rel="stylesheet" type="text/css" href="css/mainStyle.css">  
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Brett Ratner</a>
+
+
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <!-- <li>
+                        <a class="page-scroll" href="#services">Skills</a>
+                    </li> -->
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Brett Ratner</h1> <h2></br>Web Developer &</br>Full Stack Engineer</h2>
+                <hr>
+                <p>“The expert at anything was once a beginner.” -Helen Hayes</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Get to know me</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">A little about me</h2>
+                    <hr class="light">
+                    <img class="profilePic" src="img/Brett.png" alt="Brett Ratner">
+                    <p class="text-faded">I am a recent graduate from The College of New Jersey with a BA in Interactive Multimedia and a minor in Computer Science.</p>
+                    <a href="https://drive.google.com/file/d/0B-2EvHjAzlK3OC1VX3BWUUJucDQ/view?usp=sharing" target="blank" class="page-scroll btn btn-default btn-xl sr-button">My Resume</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Top Skills</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <!-- <i class="fa fa-4x fa-diamond text-primary sr-icons"></i> -->
+                        <img src="img/skills/HTML5_CSS_JavaScript.png" alt="HTML and CSS" width="150" height="100">
+                        <h3>HTML/CSS/Javascript</h3>
+                        <!-- <p class="text-muted">Our templates are updated regularly so they don't break.</p> -->
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                    <img src="img/skills/javaLogo.png" alt="HTML and CSS" width="150" height="100">
+                        <!-- <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>-->
+                        <h3>Java</h3>
+                        <!-- <p class="text-muted">You can use this theme as is, or you can make changes!</p> -->
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                    <img src="img/skills/photshopandIllistrator.png" alt="HTML and CSS" width="200" height="100">
+                        <!-- <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i> -->
+                        <h3>Photoshop/Illustrator</h3>
+                        <!-- <p class="text-muted">We update dependencies to keep things fresh.</p>-->
+                    </div> 
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                    <img src="img/skills/unity.png" alt="HTML and CSS" width="100" height="100">
+                    
+                        <!-- <i class="fa fa-4x fa-heart text-primary sr-icons"></i> -->
+                        <h3>Unity</h3>
+                        <!-- <p class="text-muted">You have to make your websites with love these days!</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Projects</h1>
+                    <!-- <small>Secondary Text</small> -->
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <div id="group1" style="display:block;">
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="img/Large/LetsGoLarge.png" data-lightbox="image-1">
+                    <img class="img-responsive" src="img/Letsgo.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h2>Let's Go</h2>
+                <h4>Tools Used: HTML, CSS, Javascript, PHP, MySQL</h4>
+                <p>A web application where once you sign in you can then see how crowded places are that have been updated by other people who were actual there. Some features are adding new locations and updating how crowded a location is if it's inaccurate. Upcoming features include a schedule builder that uses machine learning to calculate when places are busy at a certain times to then plan out your day to optimize your valuable time.</p>
+                <a class="btn btn-primary" href="thesis/index.php">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Two -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="img/Large/MedianFilterLarge.png" data-lightbox="image-2">
+                    <img class="img-responsive" src="img/medianFilter.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h2>Median photo filter</h2>
+                <h4>Tools Used: Javascript, HTML, CSS</h4>
+                <p>The median filter will reduce the noise from a photo. Using the scroll bar you can select the number of web workers you wish to perform the filter. The more times you apply the filter the less noise there will be in the photo but you will also lose some definition.</p>
+                <a class="btn btn-primary" href="Projects/MedianPhotoFilter/index.html">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Three -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="img/Large/MemoryGameLarge.png" data-lightbox="image-3">
+                    <img class="img-responsive" src="img/memoryGame.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Mix and Match memory game</h3>
+                <h4>Tools Used: Javascript, HTML, CSS</h4>
+                <p>Match the colors in the quickest time possible. There are 5 colors to match, green, blue, orange, red, and yellow, and every color has a match.</p>
+                <a class="btn btn-primary" href="Projects/MemoryGame/index.html">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Four -->
+        <div class="row">
+
+            <div class="col-md-7">
+                <a href="img/Large/MonteHallLarge.png" data-lightbox="image-4">
+                    <img class="img-responsive" src="img/MonteHall2.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Monty Hall Simulation</h3>
+                <h4>Tolls Used: Javascript, HTML, CSS</h4>
+                <p>The Monty Hall problem was a scenario on the she game show “Lets Make a Deal” where you selected one of three closed doors. Behind one of the doors was a prize and the other two doors had non-prize items or nothing at all. After you pick your door the host would then remove one of the doors that did not have the prize behind it and you were then left with the question if you would want to switch doors to the other unopened door or keep the one you originally choose. This simulation will run this scenario 10,000 times and then tell you the results of the percentage of which the player would have won if they kept their original door against if they would have switched. You may think that between the two remaining doors your chances of winner are 50 50, however this simulation shows that that is not the case. </p>
+                <a class="btn btn-primary" href="Projects/MontyHall/index.html">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Five -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="img/Large/TheFloorIsLavaLarge.png" data-lightbox="image-5">
+                    <img class="img-responsive" src="img/floorislava.png" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>The Floor is Lava</h3>
+                <h4>Tools Used: Unity, C#, MonoDevelop, Photoshop</h4>
+                <p>A platformer game where the goal is to get to the last platform in the quickest time possible. You have to avoid falling in the lava pit as well as the falling drops of lava from the ceiling. If you take too much damage you start over from the last checkpoint as well as it will add on an extra 5 seconds to your time.</p>
+                <a class="btn btn-primary" href="/Projects/TheFloorIsLava/The_Floor_Is_Lava.zip" download="TheFloorIsLava">Download Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+        <br/>
+      
+    </div>
+
+
+
+
+    <div id="group2" style="display:none;">
+     <!-- Project Six -->
+         <div class="row">
+
+            <div class="col-md-7">
+                <a href="img/Large/SoulSearcherLarge.png" data-lightbox="image-6">
+                    <img class="img-responsive" src="img/SoulSearcher.png" alt="">
+                </a>
+                
+            </div>
+            <div class="col-md-5">
+                <h3>Soul Searcher</h3>
+                <h4>Tools Used: Unity, C#, MonoDevelop, Photoshop</h4>
+                <p>Soul Searcher is a human like creation story, where you are given the chance of life however you start off as nothing. Shortly after you are given a choice on how you view the world which will determine what your soul looks like. From there it is your responsibility to create the best you, that you can make by completing the various mini games to ultimately face your greatest fear at the end of the road.</p>
+                <a class="btn btn-primary" href="/Projects/SoulSearcher/Soul_Searcher.zip" download="SoulSearcher">Download Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- <hr> -->
+
+        <!-- Project Seven -->
+        <!-- <div class="row">
+
+            <div class="col-md-7">
+                 <a href="#" data-lightbox="image-7" data-title="Created by Brett Ratner">
+                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Seven</h3>
+                <h4>Subheading</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
+                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div> -->
+        <!-- /.row -->
+
+        <!-- <hr> -->
+
+        <br/>
+      
+        </div>
+
+        <div class="paginationButtons">
+            <input class="button1" type="button" name="answer" value="1" onclick="showDiv1()" />
+            <input class="button2" type="button" name="answer" value="2" onclick="showDiv2()" />
+        </div>
+        <br/>
+        <!-- pagination -->
+        <!-- <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">&laquo;</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">&raquo;</a>
+                    </li>
+                </ul>
+            </div>
+        </div> -->
+        <!-- /.row -->
+
+     <!--    <hr> -->
+
+
+    </div>
+    <!-- /.container -->
+
+            
+        </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                
+                <!-- <a href="http://startbootstrap.com/template-overviews/creative/" class="btn btn-default btn-xl sr-button">Download Now!</a> -->
+            </div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    
+                </div>
+                <!-- <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p></p>
+                </div> -->
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p><a href="mailto:your-email@your-domain.com">brat621@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="FooterInfo">
+                        <p>Site Developed by Brett Ratner &copy;2016</p>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/scrollreveal.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+    <script src="js/toggle.js"></script>
+    <script src="js/lightbox.js"></script>
+
+</body>
+
+</html>
